2cdd0ccc383a220539488d4269221b54716226db
Alex Dang <44551719+AlexD1307@users.noreply.github.com>
2019-04-28 16:29:27 +0300

    Posts

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Post.module.css'
+
+const Post = () => {
+    return (
+                <div className={s.item}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ94_cWEa4ISg_GnyXpnYj-JZp2qY9kd4I7bVMWuVNUqz6BEj1Eg" alt=""/>
+                post 1
+                <div><span>like</span></div>
+                </div>
+    )
+}
+
+export default Post
