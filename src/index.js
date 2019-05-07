b5f8452e5b5892afe87f3430d36158cfdd5539fd
Alex Dang <44551719+AlexD1307@users.noreply.github.com>
2019-05-07 23:59:47 +0300

    create store (OOP)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state'
-import {reRenderEntireTree} from "./render";
+import store from './redux/state'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
-reRenderEntireTree(state)
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+            addPost={store.addPost.bind(store)}
+            updateNewPostText={store.updateNewPostText.bind(store)}
+            sendNewMessage={store.sendNewMessage.bind(store)}
+            addMessage={store.addMessage.bind(store)}
+            />
+        </BrowserRouter>, document.getElementById('root'));
+}
 
+rerenderEntireTree(store.getState()) //???
 
+store.subscribe(rerenderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,75 @@
-import {reRenderEntireTree} from "../render";
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hello World', likesCount: 10 },
-            { id: 2, message: 'Javascript', likesCount: 15 },
-            { id: 3, message: 'Wow', likesCount: 0 }
-        ],
-        newPostText: 'Write something'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hello World', likesCount: 10 },
+                { id: 2, message: 'Javascript', likesCount: 15 },
+                { id: 3, message: 'Wow', likesCount: 0 }
+            ],
+            newPostText: 'Write something'
+        },
+        dialogPage: {
+            dialogs: [
+                { id: 1, name: 'Alex' },
+                { id: 2, name: 'John' },
+                { id: 3, name: 'Ashley' },
+                { id: 4, name: 'Amanda' },
+                { id: 5, name: 'Christian' }
+            ],
+            messages: [
+                { id: 1, message: 'Hey' },
+                { id: 2, message: 'What\'s up' },
+                { id: 3, message: 'Javascript rules' },
+                { id: 4, message: 'Because javascript' },
+                { id: 5, message: 'Kabzda' }
+            ],
+            newMessageText: 'That\'s work'
+        }
     },
 
-    dialogPage: {
-        dialogs: [
-            { id: 1, name: 'Alex' },
-            { id: 2, name: 'John' },
-            { id: 3, name: 'Ashley' },
-            { id: 4, name: 'Amanda' },
-            { id: 5, name: 'Christian' }
-        ],
-        messages: [
-            { id: 1, message: 'Hey' },
-            { id: 2, message: 'What\'s up' },
-            { id: 3, message: 'Javascript rules' },
-            { id: 4, message: 'Because javascript' },
-            { id: 5, message: 'Kabzda' }
-        ],
-        newMessageText: 'That\'s work'
+    getState() {
+        return this._state
+    },
+
+    rerenderEntireTree() {
+        console.log('State change')
+},
+
+    addPost() {
+        let newPost = {
+            id: this._state.profilePage.posts[this._state.profilePage.posts.length - 1].id + 1,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this.rerenderEntireTree(this._state) //???
+    },
+
+    updateNewPostText(content) {
+        this._state.profilePage.newPostText = content
+        this.rerenderEntireTree(this._state)
+},
+
+    sendNewMessage(content) {
+        this._state.dialogPage.newMessageText = content
+        this.rerenderEntireTree(this._state)
+    },
+
+    addMessage() {
+        let newMessage = {
+            id: this._state.dialogPage.messages[this._state.dialogPage.messages.length - 1].id + 1,
+            message: this._state.dialogPage.newMessageText
+        }
+        this._state.dialogPage.messages.push(newMessage)
+        this._state.dialogPage.newMessageText = ''
+        this.rerenderEntireTree(this._state)
+    },
+
+    subscribe(observe) {
+        this.rerenderEntireTree = observe
     }
 }
-    
-export let addPost = () => {
-    let newPost = {
-        id: state.profilePage.posts[state.profilePage.posts.length - 1].id + 1,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    reRenderEntireTree(state)
-}
 
-export let updateNewPostText = (content) => {
-    state.profilePage.newPostText = content
-    reRenderEntireTree(state)
-}
 
-export let sendNewMessage = (content) => {
-    state.dialogPage.newMessageText = content
-    reRenderEntireTree(state)
-}
-
-export let addMessage = () => {
-    let newMessage = {
-        id: state.dialogPage.messages[state.dialogPage.messages.length - 1].id + 1,
-        message: state.dialogPage.newMessageText
-    }
-    state.dialogPage.messages.push(newMessage)
-    state.dialogPage.newMessageText = ''
-    reRenderEntireTree(state)
-}
-
-export default state
+export default store
